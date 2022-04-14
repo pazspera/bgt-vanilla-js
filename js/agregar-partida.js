d21a7fc6deedd577f1115e6a4179b640894d40b6
Paz Spera <mspera03@gmail.com>
2022-04-14 18:28:25 -0300

    agregué generar on click inputs para agregar nuevo juego

--- js/agregar-partida.js
+++ js/agregar-partida.js
@@ -1,9 +1,11 @@
 const d = document;
 const $gameList = d.getElementById("game-list");
-const $fragment = d.createDocumentFragment();
+const $btnAgregarJuego = d.getElementById("btn-agregar-juego");
+const $fieldAgregarJuego = d.getElementById("field-agregar-juego");
 
 const URL_GAMES = "./data/games.json";
 
+// Recuperar juegos disponibles de games.json
 fetch(URL_GAMES, {
   method: "GET",
 })
@@ -16,6 +18,7 @@ fetch(URL_GAMES, {
   })
   .then((json) => {
     console.log(json);
+    const $fragment = d.createDocumentFragment();
     // recorre el json y crea un option por cada juego disponible
     json.forEach((game) => {
       const $option = d.createElement("option");
@@ -30,3 +33,45 @@ fetch(URL_GAMES, {
     let message = err.statusText || "Ocurrió un error";
     console.log(message);
   });
+
+// Al hacer click en $btnAgregarJuego se generan los inputs para agregar nuevo juego
+d.addEventListener("click", (e) => {
+  if (e.target.matches("#btn-agregar-juego")) {
+    e.preventDefault();
+    // Borra contenido de $fieldAgregarJuego para evitar que se generen campos duplicados
+    $fieldAgregarJuego.textContent = "";
+
+    console.log("estoy haciendo click en el btn agregar juego");
+    // Crear input y btn de aceptar
+    const $fragment = d.createDocumentFragment();
+    const $label = d.createElement("label");
+    const $input = d.createElement("input");
+    const $aceptarBtn = d.createElement("button");
+
+    // Label
+    $label.classList.add("form-label");
+    $label.for = "nuevo-juego";
+    $label.innerHTML = "Nombre del nuevo juego";
+    $fragment.appendChild($label);
+
+    // Input
+    $input.type = "text";
+    $input.classList.add("form-control", "mb-3");
+    $input.id = "nuevo-juego";
+    $fragment.appendChild($input);
+
+    // Btn
+    $aceptarBtn.id = "guardar-juego-nuevo-btn";
+    $aceptarBtn.classList.add("btn", "btn__secondary");
+    $aceptarBtn.innerHTML = "Aceptar";
+    $fragment.appendChild($aceptarBtn);
+
+    // Agregar fragmento al DOM
+    $fieldAgregarJuego.appendChild($fragment);
+  }
+
+  // Impide que #guardar-juego-nuevo-btn refresque la página
+  if (e.target.matches("#guardar-juego-nuevo-btn")) {
+    e.preventDefault();
+  }
+});
