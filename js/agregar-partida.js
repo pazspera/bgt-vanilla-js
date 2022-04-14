574be762236a7c9fee0e14ae7c06697af5d12b65
Paz Spera <mspera03@gmail.com>
2022-04-14 17:28:50 -0300

    agregué fetch para recuperar juegos disponibles de games.json

--- js/agregar-partida.js
+++ js/agregar-partida.js
@@ -2,3 +2,31 @@ const d = document;
 const $gameList = d.getElementById("game-list");
 const $fragment = d.createDocumentFragment();
 
+const URL_GAMES = "./data/games.json";
+
+fetch(URL_GAMES, {
+  method: "GET",
+})
+  .then((res) => {
+    console.log(res);
+    return res.ok ? res.json() : Promise.reject(res);
+    // envía la respuesta al otro then en formato json si la respuesta da ok true
+    // si no está ok, fuerza a que se ejecute el catch rechazando la promesa
+    // si no ponemos el rechazo de la promesa, no se ejecuta el catch aunque haya error
+  })
+  .then((json) => {
+    console.log(json);
+    // recorre el json y crea un option por cada juego disponible
+    json.forEach((game) => {
+      const $option = d.createElement("option");
+      $option.value = game.id;
+      $option.innerHTML = game.name;
+      $fragment.appendChild($option);
+    });
+    $gameList.appendChild($fragment);
+  })
+  .catch((err) => {
+    console.log("Error desde catch");
+    let message = err.statusText || "Ocurrió un error";
+    console.log(message);
+  });
