32f721c14765b42e1a22d0cfa94bf63f94cf5df4
Paz Spera <mspera03@gmail.com>
2022-04-14 19:52:05 -0300

    agregué que se generen dinámicamente los select de jugadores en base a la cantidad de jugadores elegida por le usuarie

--- js/agregar-partida.js
+++ js/agregar-partida.js
@@ -2,8 +2,11 @@ const d = document;
 const $gameList = d.getElementById("game-list");
 const $btnAgregarJuego = d.getElementById("btn-agregar-juego");
 const $fieldAgregarJuego = d.getElementById("field-agregar-juego");
+const $selectJugadores = d.getElementById("jugadores");
+const $fieldAgregarJugadores = d.getElementById("field-agregar-jugadores");
 
 const URL_GAMES = "./data/games.json";
+const URL_PLAYERS = "./data/players.json";
 
 // Recuperar juegos disponibles de games.json
 fetch(URL_GAMES, {
@@ -76,5 +79,74 @@ d.addEventListener("click", (e) => {
     e.preventDefault();
     // Borra los campos de nuevo juego para simular que se agregó el nuevo juego al select
     $fieldAgregarJuego.textContent = "";
+  }
+});
+
+// Cuando cambia la cantidad de jugadores elegidos, crear esa cantidad de campos para elegir jugadores
+// El fetch iría dentro del for que genera los campos
+d.addEventListener("change", (e) => {
+  if (e.target.matches("#jugadores")) {
+    // borrar contenido de $fieldAgregarJugadores para evitar campos duplicados
+    $fieldAgregarJugadores.textContent = "";
+
+    let cantidadJugadores = $selectJugadores.value;
+
+    // con un for, crear los campos de cada jugador
+    // dentro del for, hacer una petición fetch para recuperar jugadores de players.json
+    for (let i = 0; i < cantidadJugadores; i++) {
+      console.log(`Jugador #${i + 1}`);
+      // Crear input para jugador
+      const $fragment = d.createDocumentFragment();
+      const $select = d.createElement("select");
+      const $label = d.createElement("label");
+
+      $label.classList.add("form-label");
+      $label.for = `jugador${i}`;
+      $label.innerHTML = `Jugador #${i + 1}`;
+      $fragment.appendChild($label);
+
+      $select.classList.add("form-select");
+      $select.id = `jugador${i}`;
+
+      // petición fetch para recuperar la lista de jugadores
+      fetch(URL_PLAYERS, {
+        method: "GET",
+      })
+        .then((res) => {
+          console.log(res);
+          return res.ok ? res.json() : Promise.reject(res);
+          // envía la respuesta al otro then en formato json si la respuesta da ok true
+          // si no está ok, fuerza a que se ejecute el catch rechazando la promesa
+          // si no ponemos el rechazo de la promesa, no se ejecuta el catch aunque haya error
+        })
+        .then((json) => {
+          console.log(json);
+          const $fragment = d.createDocumentFragment();
+
+          // genera opción default antes de recuperar players
+          const $optionDefault = d.createElement("option");
+          $optionDefault.selected;
+          $optionDefault.innerHTML = "Abrir menu para ver jugadores disponibles";
+          $fragment.appendChild($optionDefault);
+
+          // recorre el json y crea un option por cada juego disponible
+          json.forEach((player) => {
+            const $option = d.createElement("option");
+            $option.value = player.id;
+            $option.innerHTML = player.name;
+            $fragment.appendChild($option);
+          });
+          $select.appendChild($fragment);
+        })
+        .catch((err) => {
+          console.log("Error desde catch");
+          let message = err.statusText || "Ocurrió un error";
+          console.log(message);
+        });
+
+      // agregar al DOM
+      $fragment.appendChild($select);
+      $fieldAgregarJugadores.appendChild($fragment);
+    }
   }
 });
