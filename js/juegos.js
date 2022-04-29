2e513ae462a3440f2f0992ef9f1601d2d6721b1a
Paz Spera <mspera03@gmail.com>
2022-04-29 02:49:11 -0300

    en juegos agregué que cambie el título del crud cuando se hace click en los btn .edit y .delete de los juegos

--- js/juegos.js
+++ js/juegos.js
@@ -5,8 +5,10 @@
 
 const d = document;
 const $gameCrudTableContent = d.getElementById("game-crud-table-content");
-console.log($gameCrudTableContent);
-
+const $gameCrudTitle = d.getElementById("game-crud-title");
+console.log($gameCrudTitle);
+const $gameName = d.getElementById("game-name");
+console.log($gameName);
 const URL_GAMES = "./data/games.json";
 
 fetch(URL_GAMES, {
@@ -23,33 +25,33 @@ fetch(URL_GAMES, {
     const $fragment = d.createDocumentFragment();
     // recorre el json y crea un tr por cada juego
     json.forEach((game) => {
-        // crea una fila en la tabla por juego
-        const $tr = d.createElement("tr");
-        const $td = d.createElement("td");
-        $td.textContent = game.name;
+      // crea una fila en la tabla por juego
+      const $tr = d.createElement("tr");
+      const $td = d.createElement("td");
+      $td.textContent = game.name;
 
-        // crea btn para editar
-        const $td2 = d.createElement("td");
+      // crea btn para editar
+      const $td2 = d.createElement("td");
 
-        const $btnEditar = d.createElement("button");
-        $btnEditar.classList.add("edit", "btn", "btn__secondary", "me-3");
-        $btnEditar.textContent = "Editar";
-        // data attributes
-        $btnEditar.dataset.id = game.id;
-        $btnEditar.dataset.name = game.name;
-        $td2.appendChild($btnEditar);
-        
-        // crea btn para eliminar
-        const $btnEliminar = d.createElement("button");
-        $btnEliminar.classList.add("delete","btn", "btn__secondary");
-        $btnEliminar.textContent = "Eliminar";
-        // data attributes
-        $btnEliminar.dataset.id = game.id;
-        $td2.appendChild($btnEliminar);
-        
-        $tr.appendChild($td);
-        $tr.appendChild($td2);
-        $fragment.appendChild($tr);
+      const $btnEditar = d.createElement("button");
+      $btnEditar.classList.add("edit", "btn", "btn__secondary", "me-3");
+      $btnEditar.textContent = "Editar";
+      // data attributes
+      $btnEditar.dataset.id = game.id;
+      $btnEditar.dataset.name = game.name;
+      $td2.appendChild($btnEditar);
+
+      // crea btn para eliminar
+      const $btnEliminar = d.createElement("button");
+      $btnEliminar.classList.add("delete", "btn", "btn__secondary");
+      $btnEliminar.textContent = "Eliminar";
+      // data attributes
+      $btnEliminar.dataset.id = game.id;
+      $td2.appendChild($btnEliminar);
+
+      $tr.appendChild($td);
+      $tr.appendChild($td2);
+      $fragment.appendChild($tr);
     });
 
     $gameCrudTableContent.appendChild($fragment);
@@ -58,3 +60,20 @@ fetch(URL_GAMES, {
     let message = err.statusText || "Ocurrió un error";
     console.log(message);
   });
+
+d.addEventListener("click", (e) => {
+  if (e.target.matches(".edit")) {
+    e.preventDefault();
+    console.log("hola soy .edit");
+    console.log(e);
+    console.log($gameCrudTitle.innerHTML);
+    $gameCrudTitle.innerHTML = "Editar juego";
+    // $gameName.value = e.dataset.id;
+    // console.log(`dataset.id = ${e.dataset.id}`);
+  }
+
+  if (e.target.matches(".delete")) {
+    e.preventDefault();
+    $gameCrudTitle.innerHTML = "Eliminar juego";
+  }
+});
